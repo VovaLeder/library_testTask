1ad4123093a055031e29348839cb62f572a633f3
VovaLeder <bliznukvova2@gmail.com>
2022-08-21 18:37:39 +0500

    Add and Delete work. TODO: Edit

--- Program.cs
+++ Program.cs
@@ -3,15 +3,9 @@ using Library;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddRazorPages();
 
-builder.Services.AddMvc(options =>
-{
-    options.EnableEndpointRouting = false;
-});
+builder.Services.AddControllersWithViews();
 
-
-//builder.Services.AddMvc();
 builder.Services.AddSingleton<BookService>();
 builder.Services.AddCors(o => o.AddPolicy("ReactPolicy", builder =>
 {
@@ -23,20 +17,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseAuthorization();
 
 app.UseCors("ReactPolicy");
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
 
 app.Run();
